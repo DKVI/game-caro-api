c3487ea105830197d47b112b539dd46672151662
DKVI <92564346+DKVI@users.noreply.github.com>
2023-11-22 16:59:25 +0700

    add link cors

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ app.use(
 );
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://caro-game-client.vercel.app/",
+  })
+);
 app.use(xss());
 
 app.use(express.json());
